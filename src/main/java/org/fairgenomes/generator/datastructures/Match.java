e8931d3dea0e07b59a10007087ac389697832a9d
K. Joeri van der Velde <joeriv@gmail.com>
2021-10-12 16:34:22 +0200

    Introducing SKOS match types for mapping

---
+++ src/main/java/org/fairgenomes/generator/datastructures/Match.java
@@ -0,0 +1,9 @@
+package org.fairgenomes.generator.datastructures;
+
+/**
+ * See:
+ * https://www.w3.org/TR/skos-reference/#mapping
+ */
+public enum Match {
+    exactMatch, closeMatch, relatedMatch, broadMatch, narrowMatch
+}
