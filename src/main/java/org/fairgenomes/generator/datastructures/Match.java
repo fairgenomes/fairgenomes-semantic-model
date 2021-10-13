d762e1439f944be1b1af3d459ee6382003e8938c
K. Joeri van der Velde <joeriv@gmail.com>
2021-10-13 13:49:11 +0200

    Clarify match concepts

--- src/main/java/org/fairgenomes/generator/datastructures/Match.java
+++ src/main/java/org/fairgenomes/generator/datastructures/Match.java
@@ -5,5 +5,9 @@ package org.fairgenomes.generator.datastructures;
  * https://www.w3.org/TR/skos-reference/#mapping
  */
 public enum Match {
-    exactMatch, closeMatch, relatedMatch, broadMatch, narrowMatch
+    exactMatch, // Two concepts have equivalent meaning, and the link can be exploited across a wide range of applications and schemes. The link is meant to be transitive.
+    closeMatch, // Two concepts are sufficiently similar that they can be used interchangeably in many applications and schemes; this link is not meant to be transitive.
+    relatedMatch, // Associative mapping link between two concepts.
+    broadMatch, // The concept represented by the external entity is broader than the item.
+    narrowMatch // The concept represented by the external entity is narrower than the item.
 }
